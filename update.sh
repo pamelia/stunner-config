b947822157e3ca1274b0e814b5cd53f3c81ce819
Marcus Pamelia <kore@mindwipe.org>
2022-12-02 11:40:08 +0100

    added networkpolicy

--- update.sh
+++ update.sh
@@ -4,8 +4,10 @@ kubectl -n stunner-dev apply -f dev/gatewayclass.yaml
 kubectl -n stunner-dev apply -f dev/gatewayconfig.yaml
 kubectl -n stunner-dev apply -f dev/gateway.yaml
 kubectl -n stunner-dev apply -f dev/udproute.yaml
+kubectl -n stunner-dev apply -f common/networkpolicy.yaml
 
 kubectl -n stunner-prod apply -f prod/gatewayclass.yaml
 kubectl -n stunner-prod apply -f prod/gatewayconfig.yaml
 kubectl -n stunner-prod apply -f prod/gateway.yaml
 kubectl -n stunner-prod apply -f prod/udproute.yaml
+kubectl -n stunner-prod apply -f common/networkpolicy.yaml
